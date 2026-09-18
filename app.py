ac6e802de5c3c35b6e5ce0f2c03856c321002bdf
Tarra <tarra.dst@gmail.com>
2026-09-18 16:32:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,11 +90,11 @@ st.markdown("---")
 
 # SIDEBAR: MONEY MANAGEMENT & KALKULATOR LOT
 st.sidebar.header("🛡️ Proteksi Risiko & Uang")
-modal_idr = st.sidebar.number_input("Modal Saham Aktif (IDR)", min_value=0.0, value=10000000.0, step=1000000.0)
+modal_ididr = st.sidebar.number_input("Modal Saham Aktif (IDR)", min_value=0.0, value=10000000.0, step=1000000.0)
 modal_usd = st.sidebar.number_input("Modal Forex Aktif (USD)", min_value=0.0, value=1000.0, step=100.0)
 persen_risiko = st.sidebar.slider("Batas Risiko Maksimal per Trade (%)", min_value=0.5, max_value=5.0, value=1.0, step=0.5)
 
-max_risk_idr = modal_idr * (persen_risiko / 100)
+max_risk_idr = modal_ididr * (persen_risiko / 100)
 max_risk_usd = modal_usd * (persen_risiko / 100)
 st.sidebar.info(f"💡 **Batas Toleransi Los Maksimal:**\n* Saham: Rp {max_risk_idr:,.0f}\n* Forex: ${max_risk_usd:,.2f}")
 
